acf5e549d5780adc1221570b2dfd0a1851996de7
hrkrx <sebastian.leifeld@web.de>
2015-08-18 12:28:43 +0200

    First Commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApplication1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btCircular = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btCircular
+			// 
+			this.btCircular.Location = new System.Drawing.Point(71, 107);
+			this.btCircular.Name = "btCircular";
+			this.btCircular.Size = new System.Drawing.Size(50, 40);
+			this.btCircular.TabIndex = 0;
+			this.btCircular.Text = "O";
+			this.btCircular.UseVisualStyleBackColor = true;
+			this.btCircular.Click += new System.EventHandler(this.btCircular_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(595, 398);
+			this.Controls.Add(this.btCircular);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btCircular;
+	}
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		public double radoffset = 0;
+		public int stepsize = 50;
+		public double radius = 100; //px
+		public Button bt1;
+		Timer t = new Timer();
+
+		public Form1()
+		{
+			InitializeComponent();
+			t.Tick += t_Tick;
+			t.Interval = 50;
+			bt1 = btCircular;
+		}
+
+		void t_Tick(object sender, EventArgs e)
+		{
+			radoffset += (2 * Math.PI) / stepsize;
+			if (radoffset >= (2 * Math.PI)) {
+				radoffset = 0;
+			}
+			Point center = new Point(200, 200);
+			Point actualPoint = calculateCircumferencePoint(radoffset, center, radius);
+			bt1.Location = actualPoint;
+		}
+
+		private Point calculateCircumferencePoint(double radoffset, Point center, double radius)
+		{
+			Point res = new Point();
+
+			double x = center.X + radius * Math.Cos(radoffset);
+			double y = center.Y + radius * Math.Sin(radoffset);
+
+			res.X = (int)x;
+			res.Y = (int)y;
+
+			return res;
+		}
+
+		private void btCircular_Click(object sender, EventArgs e)
+		{
+			t.Start();
+		}
+	}
+}
