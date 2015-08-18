7ebf9eef405317ae8b0a59d0f01016db7aecef6a
hrkrx <sebastian.leifeld@web.de>
2015-08-18 15:24:08 +0200

    Second Commit

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,11 +28,14 @@
 		private void InitializeComponent()
 		{
 			this.btCircular = new System.Windows.Forms.Button();
+			this.btCircular2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// btCircular
 			// 
-			this.btCircular.Location = new System.Drawing.Point(71, 107);
+			this.btCircular.Location = new System.Drawing.Point(410, 258);
 			this.btCircular.Name = "btCircular";
 			this.btCircular.Size = new System.Drawing.Size(50, 40);
 			this.btCircular.TabIndex = 0;
@@ -40,21 +43,55 @@
 			this.btCircular.UseVisualStyleBackColor = true;
 			this.btCircular.Click += new System.EventHandler(this.btCircular_Click);
 			// 
+			// btCircular2
+			// 
+			this.btCircular2.Location = new System.Drawing.Point(410, 313);
+			this.btCircular2.Name = "btCircular2";
+			this.btCircular2.Size = new System.Drawing.Size(56, 46);
+			this.btCircular2.TabIndex = 1;
+			this.btCircular2.Text = "X";
+			this.btCircular2.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 346);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 376);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "label2";
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(595, 398);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btCircular2);
 			this.Controls.Add(this.btCircular);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button btCircular;
+		private System.Windows.Forms.Button btCircular2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -13,9 +13,11 @@ namespace WindowsFormsApplication1
 	public partial class Form1 : Form
 	{
 		public double radoffset = 0;
+		public double radoffset2 = Math.PI;
 		public int stepsize = 50;
 		public double radius = 100; //px
 		public Button bt1;
+		public Button bt2;
 		Timer t = new Timer();
 
 		public Form1()
@@ -24,6 +26,7 @@ namespace WindowsFormsApplication1
 			t.Tick += t_Tick;
 			t.Interval = 50;
 			bt1 = btCircular;
+			bt2 = btCircular2;
 		}
 
 		void t_Tick(object sender, EventArgs e)
@@ -32,17 +35,26 @@ namespace WindowsFormsApplication1
 			if (radoffset >= (2 * Math.PI)) {
 				radoffset = 0;
 			}
+			radoffset2 += (2 * Math.PI) / (stepsize * 2);
+			if (radoffset2 >= (2 * Math.PI)) {
+				radoffset2 = 0;
+			}
 			Point center = new Point(200, 200);
 			Point actualPoint = calculateCircumferencePoint(radoffset, center, radius);
 			bt1.Location = actualPoint;
+			Point actualPoint2 = calculateCircumferencePoint(radoffset2, bt1.Location, 50);
+			bt2.Location = actualPoint2;
+
+			label1.Text = radoffset.ToString();
+			label2.Text = (((int)radoffset) % 3).ToString();
 		}
 
 		private Point calculateCircumferencePoint(double radoffset, Point center, double radius)
 		{
 			Point res = new Point();
 
-			double x = center.X + radius * Math.Cos(radoffset);
-			double y = center.Y + radius * Math.Sin(radoffset);
+			double x = center.X +  radius * Math.Cos(radoffset);
+			double y = center.Y +  radius * Math.Sin(radoffset);
 
 			res.X = (int)x;
 			res.Y = (int)y;
